95b69bc44b2ba51ffdde399b6eff1a3cdcc113d5
m-lm <31556425+m-lm@users.noreply.github.com>
2023-04-07 18:28:24 -0500

    add load-in delay for paragraphs, and more menu item hovers

--- intro.js
+++ intro.js
@@ -1,6 +1,8 @@
 function introduce() {
-    const title = document.getElementById("curtain");
-    title.classList.toggle("introduce");
+    const c1 = document.getElementById("curtain");
+    const c2 = document.getElementById("paras");
+    c1.classList.toggle("introduce");
+    c2.classList.toggle("introduce");
 }
 
 window.addEventListener("load", introduce);
