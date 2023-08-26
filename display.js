87e9b188b88cc7d3a5102ae1312a8e20a80365c0
m-lm <31556425+m-lm@users.noreply.github.com>
2023-08-26 16:20:07 -0500

    restructure site layout, delete some things

--- display.js
+++
@@ -1,8 +0,0 @@
-function displaySection(pid) {
-    var section = document.getElementById(pid);
-    var sectionParent = document.getElementById("paras");
-    for (i = 0; i < sectionParent.children.length; i++) {
-        sectionParent.children[i].style.display = "none";
-    }
-    section.style.display = "inherit";
-}
--- intro.js
+++ intro.js
@@ -1,6 +1,6 @@
 function introduce() {
     const c1 = document.getElementById("curtain");
-    const c2 = document.getElementById("paras");
+    const c2 = document.getElementById("info");
     c1.classList.toggle("introduce");
     c2.classList.toggle("introduce");
 }
