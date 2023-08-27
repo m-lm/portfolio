c4e29be0de0b7410b8eba05baf9e1d55d44cb9be
m-lm <31556425+m-lm@users.noreply.github.com>
2023-08-27 18:15:52 -0500

    add github info, rewrite intro script

--- intro.js
+++ intro.js
@@ -1,8 +1,10 @@
 function introduce() {
     const c1 = document.getElementById("curtain");
-    const c2 = document.getElementById("info");
+    const c2 = document.getElementsByClassName("subinfo");
     c1.classList.toggle("introduce");
-    c2.classList.toggle("introduce");
+    for (var i = 0; i < c2.length; i++) {
+        c2[i].classList.toggle("introduce");
+    }
 }
 
 window.addEventListener("load", introduce);
