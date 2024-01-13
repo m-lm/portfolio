be30815a9ea0265b4c79e233085ae331aa17a79f
m-lm <31556425+m-lm@users.noreply.github.com>
2024-01-12 19:49:34 -0600

    add localStorage for theme

--- functions.js
+++ functions.js
@@ -1,4 +1,7 @@
 function introduce() {
+    if (localStorage.getItem("theme") == "dark") {
+        toggle_theme();
+    }
     const c1 = document.getElementById("curtain");
     const c2 = document.getElementsByClassName("subinfo");
     c1.classList.toggle("introduce");
@@ -9,6 +12,12 @@ function introduce() {
 
 function toggle_theme() {
     document.body.classList.toggle("dark");
+    if (document.body.className != "dark") {
+        localStorage.setItem("theme", "light");
+    }
+    else {
+        localStorage.setItem("theme", "dark");
+    }
 }
 
 window.addEventListener("load", introduce);
