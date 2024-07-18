32afb5271a6e441821b4acf60c496ec8489b3361
m-lm <31556425+m-lm@users.noreply.github.com>
2024-07-18 08:56:19 -0500

    slight tweaks

--- functions.js
+++ functions.js
@@ -1,16 +1,19 @@
 function introduce() {
     if (localStorage.getItem("theme") == "dark") {
-        toggle_theme();
+        toggleTheme();
     }
     const x = document.getElementById("curtain");
     const y = document.getElementsByClassName("subinfo");
+    const b = document.getElementById("mode");
     x.classList.toggle("introduce");
     for (let i = 0; i < y.length; i++) {
         y[i].classList.toggle("introduce");
     }
+    b.classList.toggle("introduce");
 }
 
-function toggle_theme() {
+function toggleTheme() {
+    const b = document.getElementById("mode");
     document.body.classList.toggle("dark");
     if (document.body.className != "dark") {
         localStorage.setItem("theme", "light");
